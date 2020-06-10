ee3963ed473f8172c6801c39fbac246cfbe5a537
Matt Robinson <mrmcrrobinson@gmail.com>
2020-06-10 15:03:05 +0100

    [Added] - Server and Client

---
+++ listener.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import asyncio
+import aiozmq
+import zmq
+import yaml
+
+with open("main.yml", "r") as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+protocol = cfg["RPC"]["protocol"]
+IP = cfg["RPC"]["IP"]
+port = cfg["RPC"]["port"]
+
+async def do():
+    stream = await aiozmq.stream.create_zmq_stream(
+        zmq_type=zmq.SUB, # pylint: disable=no-member
+        connect=protocol+'://'+str(IP)+':'+str(port),
+    )
+    stream.transport.subscribe(b'')
+
+    while True:
+        msg = await stream.read()
+        print(ord(msg[0]))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(do())
