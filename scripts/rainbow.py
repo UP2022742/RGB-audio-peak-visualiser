46ef35f52cdb75bedc567f88859701af104e4001
Matt Robinson <mrmcrrobinson@gmail.com>
2020-06-13 20:37:35 +0100

    [Added] - New scripts and idea for future struct.

---
+++ scripts/rainbow.py
@@ -0,0 +1,30 @@
+import board
+import neopixel
+import time
+
+pixel_pin = board.D18
+num_pixels = 50
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+def wheel(pos):
+        if pos < 0 or pos > 255:
+            return (0, 0, 0)
+        if pos < 85:
+            return (255 - pos * 3, pos * 3, 0)
+        if pos < 170:
+            pos -= 85
+            return (0, 255 - pos * 3, pos * 3)
+        pos -= 170
+        return (pos * 3, 0, 255 - pos * 3)
+    
+def rainbow_cycle(delay):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(rc_index & 255)
+        pixels.show()
+        time.sleep(delay)
+
+while True:
+    rainbow_cycle(0.0001)
